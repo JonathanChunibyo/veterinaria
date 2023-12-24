cf36363fe97edf9a0a68a1ec28a25923e07d38bc
Jonathan Rubio <jonathan.rubio@wiedii.co>
2023-12-24 00:43:23 -0500

    primera query para extraer informacion de la BD

--- register.php
+++ register.php
@@ -1,6 +1,16 @@
 <?php
 // index.php
 
+// Datos de conexión
+$servername = "localhost";
+$username = "root";
+$password = "6013078080";
+$database = "veterinaria";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Configurar CORS y encabezados JSON
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
@@ -8,11 +18,30 @@ header('Content-Type: application/json');
 
 // Verificar si es una solicitud GET
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // Obtener parámetros de la URL
-    http_response_code(200);
-    echo json_encode(array('data' => 'Paso validacion GET'));
+    $query = "SELECT * FROM servicio";
+    $result = $conn->query($query);
+    if ($result) {
+        // Obtener los resultados
+        $data = array();
+        while ($row = $result->fetch_assoc()) {
+            // Agregar cada fila de resultados al array de datos
+            $data[] = $row;
+        }
+
+        // Liberar el conjunto de resultados
+        $result->free();
+
+        // Enviar solo los datos como JSON
+        http_response_code(200);
+        echo json_encode(array('data' => $data));
+    } else {
+        http_response_code(405);
+        echo json_encode(array('error' => 'Error en la consulta'));
+    }
 } else {
     // Si la solicitud no es GET, devolver un error
     http_response_code(405);
     echo json_encode(array('error' => 'Método no permitido'));
 }
+// Cerrar conexión al finalizar operaciones en la base de datos
+$conn->close();
